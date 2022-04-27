3a9ef22d1b36c6ae2920320704d19d46e68c0713
jjiinneee <llj2653@gmail.com>
2022-04-27 19:14:59 +0900

    test

---
+++ src/test/java/com/matjo/pickafood/admin/sample/SampleTests.java
@@ -0,0 +1,19 @@
+package com.matjo.pickafood.admin.sample;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class SampleTests {
+  
+  @Test
+  public void test(){
+    log.info("---------");
+  }
+}
