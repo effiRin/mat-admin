0228bfe4a6d638b2b130a609311e080457f7172f
effiRin <effirin8@gmail.com>
2022-06-20 02:33:19 +0900

    검색 기능 수정_0619 // FoodSearchDTO extends ListDTO

--- src/main/java/com/matjo/pickafood/admin/food/controller/FoodController.java
+++ src/main/java/com/matjo/pickafood/admin/food/controller/FoodController.java
@@ -1,6 +1,5 @@
 package com.matjo.pickafood.admin.food.controller;
 
-import com.matjo.pickafood.admin.common.dto.ListDTO;
 import com.matjo.pickafood.admin.common.dto.ListResponseDTO;
 import com.matjo.pickafood.admin.common.dto.PageMaker;
 import com.matjo.pickafood.admin.food.dto.CompanyDTO;
@@ -9,16 +8,11 @@ import com.matjo.pickafood.admin.food.dto.FoodSearchDTO;
 import com.matjo.pickafood.admin.food.service.FoodService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
@@ -36,13 +30,13 @@ public class FoodController {
 
     //main
     @GetMapping("/main")
-    public void main(ListDTO listDTO, Model model) {
-        log.info(listDTO);
-        ListResponseDTO<CompanyDTO> responseDTO = foodService.getMain(listDTO);
+    public void main(FoodSearchDTO foodSearchDTO, Model model) {
+        log.info(foodSearchDTO);
+        ListResponseDTO<CompanyDTO> responseDTO = foodService.getMain(foodSearchDTO);
         model.addAttribute("companyList", responseDTO.getDtoList());
         model.addAttribute("total", responseDTO.getTotal());
         int total = responseDTO.getTotal();
-        model.addAttribute("pageMaker", new PageMaker(listDTO.getPage(), total));
+        model.addAttribute("pageMaker", new PageMaker(foodSearchDTO.getPage(), total));
 
         // 알레르기 옵션 뿌려주기
         List<String> allergyOptions = foodService.getAllergyOptions();
@@ -51,32 +45,32 @@ public class FoodController {
 
     //list 페이지
     @GetMapping("/list")
-    public void list(ListDTO listDTO, Model model) {
-        log.info(listDTO);
+    public void list(FoodSearchDTO foodSearchDTO, Model model) {
+        log.info(foodSearchDTO);
         // log.info("food list.........");
         // log.info("page : "+page);
 
         // 알레르기 체크 X, 재료명 입력 O -> 이거 줄여도 될듯??
-        if ((listDTO.getAllergy() == null) && (listDTO.getIngredient() != null)) {
+        if ((foodSearchDTO.getAllergy() == null) && (foodSearchDTO.getIngredient() != null)) {
             log.info("=======");
-            log.info("allergy is null & ingredient is " + listDTO.getIngredient());
+            log.info("allergy is null & ingredient is " + foodSearchDTO.getIngredient());
             log.info("=======");
-            ListResponseDTO<FoodDTO> responseDTO = foodService.getList(listDTO);
+            ListResponseDTO<FoodDTO> responseDTO = foodService.getList(foodSearchDTO);
             model.addAttribute("dtoList", responseDTO.getDtoList());
 //            model.addAttribute("sameFactoryList", responseDTO.get)
             model.addAttribute("total", responseDTO.getTotal());
             int total = responseDTO.getTotal();
-            model.addAttribute("pageMaker", new PageMaker(listDTO.getPage(), total));
+            model.addAttribute("pageMaker", new PageMaker(foodSearchDTO.getPage(), total));
 
         } else { // 알레르기 체크했을 경우
             log.info("=======");
             log.info("allergy!!!!");
             log.info("=======");
-            ListResponseDTO<FoodDTO> responseDTO = foodService.getAllergySearchList(listDTO);
+            ListResponseDTO<FoodDTO> responseDTO = foodService.getAllergySearchList(foodSearchDTO);
             model.addAttribute("dtoList", responseDTO.getDtoList());
             model.addAttribute("total", responseDTO.getTotal());
             int total = responseDTO.getTotal();
-            model.addAttribute("pageMaker", new PageMaker(listDTO.getPage(), total));
+            model.addAttribute("pageMaker", new PageMaker(foodSearchDTO.getPage(), total));
         }
     }
 }
--- src/main/java/com/matjo/pickafood/admin/food/mapper/FoodMapper.java
+++ src/main/java/com/matjo/pickafood/admin/food/mapper/FoodMapper.java
@@ -4,15 +4,16 @@ import com.matjo.pickafood.admin.common.dto.ListDTO;
 import com.matjo.pickafood.admin.common.mapper.GenericMapper;
 import com.matjo.pickafood.admin.food.domain.CompanyVO;
 import com.matjo.pickafood.admin.food.domain.FoodVO;
+import com.matjo.pickafood.admin.food.dto.FoodSearchDTO;
 
 import java.util.List;
 
 public interface FoodMapper extends GenericMapper<FoodVO, Integer> {
-    List<CompanyVO> companyList(ListDTO listDTO);
-    int getCompanyTotal(ListDTO listDTO);
+    List<CompanyVO> companyList(FoodSearchDTO foodSearchDTO);
+    int getCompanyTotal(FoodSearchDTO foodSearchDTO);
     List<String> allergyOptions();
 
-    List<FoodVO> allergySearchList(ListDTO listDTO);
-    int getAllergyCheckTotal(ListDTO listDTO);
+    List<FoodVO> allergySearchList(FoodSearchDTO foodSearchDTO);
+    int getAllergyCheckTotal(FoodSearchDTO foodSearchDTO);
 
 }
